f2bd1520ae9b9aab7af5d6b6b001a190b2bc8037
Simeon-JAA <118853003+Simeon-JAA@users.noreply.github.com>
2022-12-06 19:31:46 +0000

    Add files via upload
    Added new functionality project - change the colour of the body using a local array of colours & a randomly generated hex code.

---
+++ Basic Functionality (Vanilla JS)/Colour Switcher/Colourswitch.js
@@ -0,0 +1,43 @@
+//defining array of colorus
+const colours = ['Red', 'Yellow', 'Orange', 'Green', 'Blue', 'Pink', 'Purple', 'Cyan', 'Violet', 'Gray',];
+
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.colour');
+let colourText = document.querySelector('#colour-text');
+const title = document.getElementById('title');
+
+
+btn.addEventListener('click', function(){
+    //get a random number between 0 - n (legth of array)
+    let randomNumber = Math.floor(Math.random() * colours.length);
+    document.body.style.backgroundColor = colours[randomNumber];
+    colourText.innerHTML = colours[randomNumber];
+})  
+
+
+/*test function import hexCS.js
+to export back after test
+*/
+
+function randomHexColour() {
+    
+  let hexColour = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  let rnd1 = hexColour[Math.floor(Math.random() * hexColour.length)];
+  let rnd2 = hexColour[Math.floor(Math.random() * hexColour.length)];
+  let rnd3 = hexColour[Math.floor(Math.random() * hexColour.length)];
+  let rnd4 = hexColour[Math.floor(Math.random() * hexColour.length)];
+  let rnd5 = hexColour[Math.floor(Math.random() * hexColour.length)];
+  let rnd6 = hexColour[Math.floor(Math.random() * hexColour.length)];
+
+
+  let rndGenHex = '#' + rnd1 + rnd2 + rnd3 + rnd4 + rnd5 + rnd6;
+  
+  console.log(rndGenHex);
+
+  document.body.style.backgroundColor = rndGenHex;
+
+  const colourText = document.querySelector('#colour-text');
+
+  colourText.textContent = ' ' + rndGenHex;
+}
